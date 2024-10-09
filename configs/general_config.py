7258cba2efab93bd6f16a8fb2dcdaad78bb7d09d
mekaneeky <alizawahry1@gmail.com>
2024-10-10 01:15:41 +0300

    Update general_config.py

--- configs/general_config.py
+++ configs/general_config.py
@@ -1,7 +1,7 @@
 class GeneralConfig:
     device = "cpu"  # Use "cuda" for GPU, "cpu" for CPU
-    hf_token = "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"
-    gene_repo = "mekaneeky/testing-repo-4"
+    hf_token = "enter_your_hf_repo_here"
+    gene_repo = "your_username/your_repo"
     metrics_file = "metrics.csv"
     seed = 42
     # Hugging Face Configuration
