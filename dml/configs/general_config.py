6325efc6c7559bd433b04cfe694320ca12904902
mekaneeky <alizawahry1@gmail.com>
2024-10-10 09:19:00 +0300

    Update general_config.py

--- dml/configs/general_config.py
+++ dml/configs/general_config.py
@@ -1,6 +1,6 @@
 class GeneralConfig:
     device = "cpu"  # Use "cuda" for GPU, "cpu" for CPU
-    hf_token = "enter_your_hf_repo_here"
+    hf_token = "enter_your_hf_token_here"
     gene_repo = "your_username/your_repo"
     metrics_file = "metrics.csv"
     seed = 42
