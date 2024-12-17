fad4325274ced1f163eddaf09f8d12f2ab77a44d
mekaneeky <alizawahry1@gmail.com>
2024-12-17 21:29:00 +0300

    Update general_config.py

--- dml/configs/general_config.py
+++ dml/configs/general_config.py
@@ -1,5 +1,5 @@
 class GeneralConfig:
-    device = "cpu"  # Use "cuda" for GPU, "cpu" for CPU
+    device = "cuda"  # Use "cuda" for GPU, "cpu" for CPU
     hf_token = "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"
     gene_repo = "mekaneeky/testing-repo-7"
     metrics_file = "metrics.csv"
