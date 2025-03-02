3f8c55d62ce79baf563965ea2561a6a500066a3a
wenknow <wc9241@gmail.com>
2025-03-02 21:10:57 +0800

    fix

--- dml/configs/general_config.py
+++ dml/configs/general_config.py
@@ -1,5 +1,5 @@
 class GeneralConfig:
-    device = "cpu"  # Use "cuda" for GPU, "cpu" for CPU
+    device = "cuda"  # Use "cuda" for GPU, "cpu" for CPU
     hf_token = "your_hf_token"
     gene_repo = "your_hf_repo"
     metrics_file = "metrics.csv"
